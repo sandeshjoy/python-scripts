f053c0d460c326caeeadebb8282bd7686beb9e8e
raunakkunwar <55635294+raunakkunwar@users.noreply.github.com>
2020-10-14 13:32:54 -0500

    Added function that adds items to a dictionary

---
+++ dict.py
@@ -0,0 +1,11 @@
+def add_items(some_dict):
+  # Add new items to dict
+  some_dict['k'] = 3
+  some_dic['k'] = 4
+  return some_dict
+  
+  
+if __name__ == __main__:
+  some_dict = {'a': 1, 'b': 2}
+  new_dict = add_items()
+  print(new_dict)
