db30d87a7fd2a9ceb32c3f0f2b46f8ff8296df81
Sandy Vaii <55762127+sandeshjoy@users.noreply.github.com>
2020-10-14 22:02:17 -0500

    add sum function
    Hey Mr. Kunwar, I guess adding a sum function to your code would enhance its applicability, please let me know if you liked it or not?

--- dict.py
+++ dict.py
@@ -2,6 +2,11 @@ def add_items(some_dict):
   # Add new items to dict
   some_dict.update({'k': 3, 'l': 4})
   return some_dict
+
+def sum(a,b):
+  # summation of two numbers
+  sum = a+b-(a+2*b)
+  return sum
   
   
 if __name__ == __main__:
